5067ade1cad323509a73c4b1790b59f1bd4b744e
baymaxxx <81243232@qq.com>
2017-01-03 20:08:41 +0800

    header.vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -21,8 +21,38 @@
     <div class="bulletin-wrapper"></div>
   </div>
 </template>
-<style>
+<style lang="scss" rel="stylesheet/scss">
+  @import "../../common/sass/mixin";
+  .header{
+    background-color: #000;
+    color: #fff;
+    .content-wrapper{
+      padding: 24px 12px 18px 24px;
+      font-style: 0;
+      .avatar{
+        display: inline-block;
+      }
+      .content{
+        display: inline-block;
+        margin-left:16px;
+        font-size: 14px;
+        .title{
+          margin: 2px 0 8px;
+          .brand{
+            display: inline-block;
+            width: 30px;
+            height: 18px;
+            @include bg-image("brand");
+            background-size: 30px 18px;
+            background-repeat: no-repeat;
+            .name{
 
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
 <script>
   export default{
