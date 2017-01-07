ba7b2101180b0d59a3f8c800d5e22b64064535fb
baymaxxx <81243232@qq.com>
2017-01-07 22:15:54 +0800

    star.vue

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -99,7 +99,19 @@
       },
       itemClasses() {
         let result = [];
-//        let score = Math.floor()
+        let score = Math.floor(this.score * 2) / 2;
+        let hasDecimal = score % 1 !== 0;
+        let integer = Math.floor(score);
+        for(let i = 0; i < integer; i++){
+            result.push(CLS_ON);
+        }
+        if(hasDecimal){
+            result.push(CLS_HALF);
+        }
+        while (result.length < LENGTH){
+            result.push(CLS_OFF)
+        }
+        return result;
       }
     }
   };
